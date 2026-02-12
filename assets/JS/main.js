3a391f9116594650ee37f7b2fb79d47b2f7e57a9
Francesca Golzio <francescagolzio@gmail.com>
2026-02-12 17:15:43 +0100

    add btn interaction

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,16 +1,23 @@
 /* is it working? */
-console.log("🟢 it's working...")
+//console.log("🟢 it's working...")
 
 
+/* Raccolta dati */
 
+const nameEl = document.getElementById('name');
+const kmEl = document.getElementById('km');
+const ageEl = document.getElementById('age');
+const btnEl = document.querySelector('button');
 
+console.log(nameEl, kmEl, ageEl);
 
+/* Attivare il bottone */
 
+btnEl.addEventListener('submit', function(e) {
+  e.preventDefault();
+  console.log(nameEl.value, kmEl.value, ageEl.value);
 
-
-
-
-
+})
 
 
 
